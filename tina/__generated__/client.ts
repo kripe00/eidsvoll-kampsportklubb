b51de641377ccd6c1bdef87607cef49da7a79346
Kristian <kristianpedersen00@hotmail.com>
2026-04-29 12:38:54 +0200

    Fix TinaCMS production assets loading

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
+export const client = createClient({ cacheDir: '/Users/kristianpedersen/Eidsvoll kampsportklubb/tina/__generated__/.cache/1777459102061', url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
