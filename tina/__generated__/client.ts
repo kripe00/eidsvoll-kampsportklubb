f742a1909cd9570fcea0de5751bc40704dde0b19
Kristian <kristianpedersen00@hotmail.com>
2026-05-03 18:43:20 +0200

    Standard production build confirmed working and deployed

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/kristianpedersen/Eidsvoll kampsportklubb/tina/__generated__/.cache/1777826102039', url: 'https://content.tinajs.io/2.2/content/810c085c-dd16-47c3-8ca2-865f4019e4ca/github/main', token: '8239d33b0e1a5811c2adb69d259f0009892f6f12', queries,  });
+export const client = createClient({ cacheDir: '/Users/kristianpedersen/Eidsvoll kampsportklubb/tina/__generated__/.cache/1777826560347', url: 'https://content.tinajs.io/2.2/content/810c085c-dd16-47c3-8ca2-865f4019e4ca/github/main', token: 'c05f5e9a3f49d2bf637d361a9384fb627a783e6e', queries,  });
 export default client;
   
