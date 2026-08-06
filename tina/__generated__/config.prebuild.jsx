88dd923450074fafbd1e0e69cb2f98bb5064dc86
Kristian <kristianpedersen00@hotmail.com>
2026-08-06 10:27:52 +0200

    fix: legg til clientId og token fallbacks i tina/config.ts

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -3,8 +3,8 @@ import { defineConfig } from "tinacms";
 var branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 var config_default = defineConfig({
   branch,
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
-  token: process.env.TINA_TOKEN || "",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "810c085c-dd16-47c3-8ca2-865f4019e4ca",
+  token: process.env.TINA_TOKEN || "c05f5e9a3f49d2bf637d361a9384fb627a783e6e",
   build: {
     outputFolder: "admin",
     publicFolder: "public"
