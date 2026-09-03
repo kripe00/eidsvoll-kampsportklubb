b6b57cfa99ed4082eb7847932c4456914152d401
Kristian <kristianpedersen00@hotmail.com>
2026-09-03 17:02:40 +0200

    fix(hosting): oppdatere cache-control for admin til no-cache

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/Users/kristianpedersen/Eidsvoll kampsportklubb/tina/__generated__/.cache/1788447001140', url: 'https://content.tinajs.io/2.4/content/810c085c-dd16-47c3-8ca2-865f4019e4ca/github/main', token: 'c05f5e9a3f49d2bf637d361a9384fb627a783e6e', queries,  });
+export const client = createClient({ cacheDir: '/Users/kristianpedersen/Eidsvoll kampsportklubb/tina/__generated__/.cache/1788447722471', url: 'https://content.tinajs.io/2.4/content/810c085c-dd16-47c3-8ca2-865f4019e4ca/github/main', token: 'c05f5e9a3f49d2bf637d361a9384fb627a783e6e', queries,  });
 export default client;
   
